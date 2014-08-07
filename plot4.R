28c1814c7a997a8c0a0157cc22176eef6ece9f5c
Alberto Gurpegui <gurpegui.alberto@gmail.com>
2014-08-08 00:36:26 +0200

    complete commit of 4 R codes and 4 png plots

--- plot4.R
+++ plot4.R
@@ -1,4 +1,4 @@
-plot3 <- function(){
+plot4 <- function(){
       my_file <- file("household_power_consumption.txt","rt")
       data_aux <- read.table(my_file, 
                              header = TRUE, sep = ";", numerals = "no.loss", na.strings = "?", 
@@ -17,13 +17,25 @@ plot3 <- function(){
       data <- rbind(data_tmp1, data_tmp2)
       data$Time <- strptime(paste(data$Date, data$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
       data$Date <- as.Date(data$Date,"%d/%m/%Y")
-      png(filename = "plot3.png", width = 480, height = 480)
+      png(filename = "plot4.png", width = 480, height = 480)
+      #plots will be drawn by columns
+      par(mfcol=c(2,2))
+      #cex: amount by which plotting text and symbols should be magnified relative to the default
+      #cex.lab: The magnification to be used for x and y labels relative to the current setting of cex.
+      par(cex = 0.8, cex.lab = 0.9)
+      #first plot
+      with(data, plot(Time, Global_active_power, type = "l", xlab = "", ylab = "Global Active Power"))
+      #second plot
       with(data, plot(Time, Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering"))
       plot_colors <- c("black","red","blue")
       names <- colnames(data)[7:9]
       lines(data$Time, data$Sub_metering_2, col = plot_colors[2] )
       lines(data$Time, data$Sub_metering_3, col = plot_colors[3])
-      legend("topright", legend=names, col=plot_colors, cex = 0.9, lty=1)
+      #lty adds little line to each legend. bty = "n" remove legend's box 
+      legend("topright", legend=names, col=plot_colors, lty=1, bty="n")
+      #third plot
+      with(data, plot(Time, Voltage, type = "l", xlab="datetime", ylab="Voltage"))
+      #fourth plot
+      with(data, plot(Time, Global_reactive_power, type = "l", xlab = "datetime", ylab = "Global_reactive_power"))
       dev.off()
-      
 }
